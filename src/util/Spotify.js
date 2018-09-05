6619a411b47607515b9a4efb4150badf51423400
Nonu Singh <nonuatamd@gmail.com>
2018-09-05 13:43:53 -0500

    Add files via upload

---
+++ src/util/Spotify.js
@@ -0,0 +1,100 @@
+let accessToken;
+const clientId = '1fc23b9f96ec46eda31d16b85a768257';
+const redirectUri = 'http://localhost:3000/'
+const Spotify = {
+
+  getAccessToken() {
+    if(accessToken) {
+      return accessToken;
+    }
+    const newAccessToken = window.location.href.match(/access_token=([^&]*)/);
+    const newExpiresIn = window.location.href.match(/expires_in=([^&]*)/);
+    if (newAccessToken && newExpiresIn) {
+      accessToken = newAccessToken[1];
+      const expiresIn = Number(newExpiresIn[1]);
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&show_dialog=true&redirect_uri=${redirectUri}`;
+      window.location = accessUrl;
+    }
+  },
+
+  search(term) {
+    const accessToken = Spotify.getAccessToken();
+    const headers = {Authorization: `Bearer ${accessToken}`};
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,
+      headers: headers).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  }, networkError => {
+    console.log(networkError.message);
+  }).then(jsonResponse => {
+      if (!jsonResponse.tracks) {
+        return [];
+      }
+      return jsonResponse.tracks.items.map(track => ({
+        id: track.id,
+        name: track.name,
+        artist: track.artists[0].name,
+        album: track.album.name,
+        uri: track.uri}));
+    });
+  },
+
+  savePlaylist(playlistName, trackURIs) {
+    if (playlistName && trackURIs.length) {
+      const accessToken = Spotify.getAccessToken();
+      const headers = {
+        Authorization: `Bearer ${accessToken}`
+      };
+      let userID;
+      let playlistID;
+      return fetch('https://api.spotify.com/v1/me', {headers: headers}).then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Request failed!');
+      }, networkError => {
+        console.log(networkError.message);
+      }).then(jsonResponse => {
+        userID = jsonResponse.id;
+        return fetch(`https://api.spotify.com/v1/users/${userID}/playlists`, {
+          method: 'POST',
+          headers: headers,
+          body: JSON.stringify({name: playlistName})
+        }).then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error('Request failed!');
+        }, networkError => {
+          console.log(networkError.message);
+        }).then(jsonResponse => {
+          playlistID = jsonResponse.id;
+          return fetch(`https://api.spotify.com/v1/users/${userID}/playlists/${playlistID}/tracks`, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify({uris: trackURIs})
+          }).then(response => {
+            if (response.ok) {
+              return response.json();
+            }
+            throw new Error('Request failed!');
+          }, networkError => {
+            console.log(networkError.message);
+          }).then(jsonResponse => jsonResponse);
+        });
+      });
+
+    } else {
+      return;
+    }
+  }
+}
+
+
+export default Spotify;
